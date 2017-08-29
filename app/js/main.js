24b03fcced51a8e454c4b369c8185805d9734e2e
alexrewrew <alexrewrew@MacBook-Pro-Oleksandr.local>
2017-08-29 16:20:31 +0300

    Update 29.08

--- app/js/main.js
+++ app/js/main.js
@@ -34465,7 +34465,35 @@ $(document).ready(function () {
         autoplaySpeed:1500,
         dots: true,
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        // responsive: [
+        //     {
+        //         breakpoint: 1024,
+        //         settings: {
+        //             slidesToShow: 2,
+        //             slidesToScroll: 1,
+        //             infinite: false,
+        //         }
+        //     },
+        //     {
+        //         breakpoint: 600,
+        //         settings: {
+        //             slidesToShow: 2,
+        //             slidesToScroll: 2
+        //         }
+        //     },
+        //     {
+        //         breakpoint: 480,
+        //         settings: {
+        //             slidesToShow: 1,
+        //             slidesToScroll: 1
+        //         }
+        //     }
+        //     // You can unslick at a given breakpoint now by adding:
+        //     // settings: "unslick"
+        //     // instead of a settings object
+        // ]
+
     });
 
     $('.slider-article-big').slick({
